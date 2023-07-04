12785979c0e06a277f962eaac92ea18237eef93e
hau <hau.tvc@intelin.vn>
2023-07-04 19:15:15 +0700

    add field to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :jwt_authenticatable,
          jwt_revocation_strategy: JwtDenylist
+
+  validates_presence_of :name, :email, :username, :encrypted_password
 end
