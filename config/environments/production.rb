4928c21c76271248bf1c1ec80136fb2e1a9cb7ed
hau <hau.tvc@intelin.vn>
2023-08-19 10:45:05 +0700

    fix configure action cable on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,9 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
-  config.action_cable.allowed_request_origins = ['https://api-meepo.onrender.com']
+  #
+  config.action_cable.mount_path = '/cable'
+  config.action_cable.allowed_request_origins = ['https://meepo-app.onrender.com']
   config.action_cable.allowed_request_origins = [/http:\/\/.*/, /https:\/\/.*/]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
